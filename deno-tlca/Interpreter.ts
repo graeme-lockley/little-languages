d864d10c2fdffb474f30a8575e2534089bb4a398
Graeme Lockley <graeme.lockley@gmail.com>
2022-12-10 13:43:24 +0200

    deno-tlca: refactor: tidy up usage of any

--- deno-tlca/Interpreter.ts
+++ deno-tlca/Interpreter.ts
@@ -1,4 +1,3 @@
-// deno-lint-ignore-file no-explicit-any
 import { Constraints } from "./Constraints.ts";
 import { inferExpression } from "./Infer.ts";
 import {
@@ -24,115 +23,25 @@ import {
   typeUnit,
 } from "./Typing.ts";
 
-const binaryOps = new Map<number, (v1: any, v2: any) => any>([
-  [Op.Equals, (a, b) => a === b],
-  [Op.Plus, (a, b) => (a + b) | 0],
-  [Op.Minus, (a, b) => (a - b) | 0],
-  [Op.Times, (a, b) => (a * b) | 0],
-  [Op.Divide, (a, b) => (a / b) | 0],
-]);
+// deno-lint-ignore no-explicit-any
+type RuntimeValue = any;
 
-const evaluate = (expr: Expression, env: any): any => {
-  if (expr.type === "App") {
-    const operator = evaluate(expr.e1, env);
-    const operand = evaluate(expr.e2, env);
-    return operator(operand);
-  }
-  if (expr.type === "If") {
-    return evaluate(expr.guard, env)
-      ? evaluate(expr.then, env)
-      : evaluate(expr.else, env);
-  }
-  if (expr.type === "Lam") {
-    return (x: any): any => {
-      const newEnv = { ...env };
-      newEnv[expr.name] = x;
-      return evaluate(expr.expr, newEnv);
-    };
-  }
-  if (expr.type === "Let" || expr.type === "LetRec") {
-    return executeDeclaration(expr, env)[0];
-  }
-  if (expr.type === "LBool") {
-    return expr.value;
-  }
-  if (expr.type === "LInt") {
-    return expr.value;
-  }
-  if (expr.type === "LString") {
-    return expr.value;
-  }
-  if (expr.type === "LTuple") {
-    return expr.values.map((v) => evaluate(v, env));
-  }
-  if (expr.type === "LUnit") {
-    return null;
-  }
-  if (expr.type === "Match") {
-    const e = evaluate(expr.expr, env);
+type RuntimeEnv = { [key: string]: RuntimeValue };
 
-    for (const c of expr.cases) {
-      const newEnv = matchPattern(c.pattern, e, env);
-      if (newEnv !== null) {
-        return evaluate(c.expr, newEnv);
-      }
-    }
-    throw new Error("Match failed");
-  }
-  if (expr.type === "Op") {
-    const left = evaluate(expr.left, env);
-    const right = evaluate(expr.right, env);
-    return binaryOps.get(expr.op)!(left, right);
-  }
-  if (expr.type === "Var") {
-    return env[expr.name];
-  }
+export const runtime = (env: Env): RuntimeEnv => env[0];
 
-  return null;
-};
+export const typeEnv = (env: Env): TypeEnv => env[1];
 
-const matchPattern = (pattern: Pattern, value: any, env: any): any => {
-  if (pattern.type === "PBool") {
-    return pattern.value === value ? env : null;
-  }
-  if (pattern.type === "PInt") {
-    return pattern.value === value ? env : null;
-  }
-  if (pattern.type === "PString") {
-    return pattern.value === value ? env : null;
-  }
-  if (pattern.type === "PVar") {
-    const newEnv = { ...env };
-    newEnv[pattern.name] = value;
-    return newEnv;
-  }
-  if (pattern.type === "PTuple") {
-    let newEnv = env;
-    for (let i = 0; i < pattern.values.length; i++) {
-      newEnv = matchPattern(pattern.values[i], value[i], newEnv);
-      if (newEnv === null) {
-        return null;
-      }
-    }
-    return newEnv;
-  }
-  if (pattern.type === "PUnit") {
-    return value === null ? env : null;
-  }
-  if (pattern.type === "PWildcard") {
-    return env;
-  }
-  return null;
-};
-
-export type Env = [any, TypeEnv];
+export type Env = [RuntimeEnv, TypeEnv];
 
 const mkEnv = (
-  runtime: any,
+  runtime: RuntimeEnv,
   typeEnv: TypeEnv,
 ): Env => [runtime, typeEnv];
 
-export const emptyEnv = mkEnv({}, emptyTypeEnv);
+export const emptyRuntimeEnv: RuntimeEnv = {};
+
+export const emptyEnv = mkEnv(emptyRuntimeEnv, emptyTypeEnv);
 
 export const defaultEnv = mkEnv(
   {
@@ -167,40 +76,145 @@ export const defaultEnv = mkEnv(
     ),
 );
 
-export const runtime = (env: Env): any => env[0];
-export const typeEnv = (env: Env): TypeEnv => env[1];
+const binaryOps = new Map<
+  number,
+  (v1: RuntimeValue, v2: RuntimeValue) => RuntimeValue
+>([
+  [Op.Equals, (a, b) => a === b],
+  [Op.Plus, (a, b) => (a + b) | 0],
+  [Op.Minus, (a, b) => (a - b) | 0],
+  [Op.Times, (a, b) => (a * b) | 0],
+  [Op.Divide, (a, b) => (a / b) | 0],
+]);
+
+const evaluate = (expr: Expression, runtimeEnv: RuntimeEnv): RuntimeValue => {
+  if (expr.type === "App") {
+    const operator = evaluate(expr.e1, runtimeEnv);
+    const operand = evaluate(expr.e2, runtimeEnv);
+    return operator(operand);
+  }
+  if (expr.type === "If") {
+    return evaluate(expr.guard, runtimeEnv)
+      ? evaluate(expr.then, runtimeEnv)
+      : evaluate(expr.else, runtimeEnv);
+  }
+  if (expr.type === "Lam") {
+    return (x: RuntimeValue): RuntimeValue => {
+      const newRuntimeEnv = { ...runtimeEnv };
+      newRuntimeEnv[expr.name] = x;
+      return evaluate(expr.expr, newRuntimeEnv);
+    };
+  }
+  if (expr.type === "Let" || expr.type === "LetRec") {
+    return executeDeclaration(expr, runtimeEnv)[0];
+  }
+  if (expr.type === "LBool") {
+    return expr.value;
+  }
+  if (expr.type === "LInt") {
+    return expr.value;
+  }
+  if (expr.type === "LString") {
+    return expr.value;
+  }
+  if (expr.type === "LTuple") {
+    return expr.values.map((v) => evaluate(v, runtimeEnv));
+  }
+  if (expr.type === "LUnit") {
+    return null;
+  }
+  if (expr.type === "Match") {
+    const e = evaluate(expr.expr, runtimeEnv);
+
+    for (const c of expr.cases) {
+      const newEnv = matchPattern(c.pattern, e, runtimeEnv);
+      if (newEnv !== null) {
+        return evaluate(c.expr, newEnv);
+      }
+    }
+    throw new Error("Match failed");
+  }
+  if (expr.type === "Op") {
+    const left = evaluate(expr.left, runtimeEnv);
+    const right = evaluate(expr.right, runtimeEnv);
+    return binaryOps.get(expr.op)!(left, right);
+  }
+  if (expr.type === "Var") {
+    return runtimeEnv[expr.name];
+  }
+
+  return null;
+};
+
+const matchPattern = (
+  pattern: Pattern,
+  value: RuntimeValue,
+  runtimeEnv: RuntimeEnv,
+): RuntimeEnv | null => {
+  if (pattern.type === "PBool") {
+    return pattern.value === value ? runtimeEnv : null;
+  }
+  if (pattern.type === "PInt") {
+    return pattern.value === value ? runtimeEnv : null;
+  }
+  if (pattern.type === "PString") {
+    return pattern.value === value ? runtimeEnv : null;
+  }
+  if (pattern.type === "PVar") {
+    const newEnv = { ...runtimeEnv };
+    newEnv[pattern.name] = value;
+    return newEnv;
+  }
+  if (pattern.type === "PTuple") {
+    let newRuntimeEnv: RuntimeEnv | null = runtimeEnv;
+    for (let i = 0; i < pattern.values.length; i++) {
+      newRuntimeEnv = matchPattern(pattern.values[i], value[i], newRuntimeEnv);
+      if (newRuntimeEnv === null) {
+        return null;
+      }
+    }
+    return newRuntimeEnv;
+  }
+  if (pattern.type === "PUnit") {
+    return value === null ? runtimeEnv : null;
+  }
+  if (pattern.type === "PWildcard") {
+    return runtimeEnv;
+  }
+  return null;
+};
 
 const executeDeclaration = (
   expr: LetExpression | LetRecExpression,
-  env: any,
-): [any, any] => {
-  const newEnv = { ...env };
-  const values: Array<any> = [];
+  runtimeEnv: RuntimeEnv,
+): [RuntimeValue, RuntimeEnv] => {
+  const newRuntimeEnv = { ...runtimeEnv };
+  const values: Array<RuntimeValue> = [];
+
   expr.declarations.forEach((d) => {
-    const value = evaluate(d.expr, newEnv);
-    newEnv[d.name] = value;
+    const value = evaluate(d.expr, newRuntimeEnv);
+    newRuntimeEnv[d.name] = value;
     values.push(value);
   });
 
-  if (expr.expr === undefined) {
-    return [values, newEnv];
-  } else {
-    return [evaluate(expr.expr, newEnv), env];
-  }
+  return (expr.expr === undefined)
+    ? [values, newRuntimeEnv]
+    : [evaluate(expr.expr, newRuntimeEnv), runtimeEnv];
 };
 
-const executeExpression = (expr: Expression, env: any): [any, any] => {
-  if (expr.type === "Let" || expr.type === "LetRec") {
-    return executeDeclaration(expr, env);
-  }
-  return [evaluate(expr, env), env];
-};
+const executeExpression = (
+  expr: Expression,
+  runtimeEnv: RuntimeEnv,
+): [RuntimeValue, RuntimeEnv] =>
+  (expr.type === "Let" || expr.type === "LetRec")
+    ? executeDeclaration(expr, runtimeEnv)
+    : [evaluate(expr, runtimeEnv), runtimeEnv];
 
 export const executeProgram = (
   program: Program,
   env: Env,
-): [Array<[any, Type]>, Env] => {
-  const results: Array<[any, Type]> = [];
+): [Array<[RuntimeValue, Type]>, Env] => {
+  const results: Array<[RuntimeValue, Type]> = [];
   const pump = createFresh();
 
   program.forEach((e) => {
@@ -217,9 +231,9 @@ export const executeProgram = (
     const subst = constraints.solve();
     const newType = type.apply(subst);
 
-    const [v, newRuntime] = executeExpression(e, runtime(env));
+    const [value, newRuntime] = executeExpression(e, runtime(env));
 
-    results.push([v, newType]);
+    results.push([value, newType]);
     env = mkEnv(newRuntime, newTypeEnv);
   });
 
@@ -227,15 +241,11 @@ export const executeProgram = (
 };
 
 export const execute = (
-  t: string,
-  env: Env = [{}, emptyTypeEnv],
-): [Array<[any, Type]>, Env] => {
-  const ast = parse(t);
+  input: string,
+  env: Env = emptyEnv,
+): [Array<[RuntimeValue, Type]>, Env] => executeProgram(parse(input), env);
 
-  return executeProgram(ast, env);
-};
-
-export const valueToString = (v: any, type: Type): string => {
+export const valueToString = (v: RuntimeValue, type: Type): string => {
   if (type === typeUnit) {
     return "()";
   }
@@ -247,7 +257,8 @@ export const valueToString = (v: any, type: Type): string => {
   }
   if (type instanceof TTuple) {
     return `(${
-      v.map((v: any, i: number) => valueToString(v, type.types[i])).join(", ")
+      v.map((v: RuntimeValue, i: number) => valueToString(v, type.types[i]))
+        .join(", ")
     })`;
   }
   return `${v}`;
@@ -256,23 +267,15 @@ export const valueToString = (v: any, type: Type): string => {
 export type NestedString = string | Array<NestedString>;
 
 export const expressionToNestedString = (
-  value: any,
+  value: RuntimeValue,
   type: Type,
-  e: Expression,
-): NestedString => {
-  if ((e.type === "Let" || e.type === "LetRec") && type instanceof TTuple) {
-    return e.declarations.map((d, i) =>
+  expr: Expression,
+): NestedString =>
+  ((expr.type === "Let" || expr.type === "LetRec") && type instanceof TTuple)
+    ? expr.declarations.map((d, i) =>
       `${d.name} = ${valueToString(value[i], type.types[i])}: ${type.types[i]}`
-    );
-  } else {
-    return `${valueToString(value, type)}: ${type}`;
-  }
-};
+    )
+    : `${valueToString(value, type)}: ${type}`;
 
-export const nestedStringToString = (s: NestedString): string => {
-  if (Array.isArray(s)) {
-    return s.map(nestedStringToString).join("\n");
-  } else {
-    return s;
-  }
-};
+export const nestedStringToString = (s: NestedString): string =>
+  Array.isArray(s) ? s.map(nestedStringToString).join("\n") : s;
