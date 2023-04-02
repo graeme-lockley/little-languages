c756d3a0d32ce47df3b92adba1f5031a4c1df158
Graeme Lockley <graeme.lockley@gmail.com>
2023-04-02 08:44:50 +0200

    refactor: move tlca into it's own project

--- deno-tlca/Parser.test.ts
+++
@@ -1,7 +0,0 @@
-import { assertEquals } from "https://deno.land/std@0.137.0/testing/asserts.ts";
-import { transformLiteralString } from "./Parser.ts";
-
-Deno.test("transformLiteralString", () => {
-  assertEquals(transformLiteralString('"foo"'), "foo");
-  assertEquals(transformLiteralString('"\\"foo\\""'), '"foo"');
-});
